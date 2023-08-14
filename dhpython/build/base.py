31eff8fd63d9e123e4d75423d1af8b2d6a188d36
Stuart Prescott <stuart@debian.org>
2023-08-14 14:09:21 +1000

    Remove *.egg-info directories in clean step
    Removing the egg-info directory automatically in the 'clean' step means that
    if the build changes the egg-info, the change will not trip up the next
    build of the source package.
    
    * if the egg-info directory is generated by the build, then removing it
      is the right thing to do
    
    * if the egg-info directory is included in the upstream source, then
      deleting it causes no harm as dpkg-source warns on missing files in
      the source package but does not exit with error.

--- dhpython/build/base.py
+++ dhpython/build/base.py
@@ -185,7 +185,7 @@ class Base:
 
         for root, dirs, file_names in walk(context['dir']):
             for name in dirs:
-                if name == '__pycache__':
+                if name == '__pycache__' or name.endswith('egg-info'):
                     dpath = join(root, name)
                     log.debug('removing dir: %s', dpath)
                     try:
