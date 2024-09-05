048976a62124123ef1edc66c075b589605a13c57
Stefano Rivera <stefanor@debian.org>
2024-09-05 18:12:57 +0200

    pybuild: Stop ignoring an exit code of 5, for no tests found. (Closes: #1024971)

--- dhpython/build/base.py
+++ dhpython/build/base.py
@@ -282,9 +282,6 @@ class Base:
         elif self.cfg.test_custom:
             return 'cd {build_dir}; {args}'
         else:
-            # Temporary: Until Python 3.12 is established, and packages without
-            # test suites have explicitly disabled tests.
-            args['ignore_no_tests'] = True
             return 'cd {build_dir}; {interpreter} -m unittest discover -v {args}'
 
     def build_wheel(self, context, args):
@@ -355,10 +352,7 @@ def shell_command(func):
         command = command.format(**quoted_args)
 
         output = self.execute(context, args, command, log_file)
-        if output['returncode'] == 5 and args.get('ignore_no_tests', False):
-            # Temporary hack (see Base.test)
-            pass
-        elif output['returncode'] != 0:
+        if output['returncode'] != 0:
             msg = 'exit code={}: {}'.format(output['returncode'], command)
             if log_file:
                 msg += '\nfull command log is available in {}'.format(log_file)
